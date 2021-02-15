af6cebb3f168c6829ea4c0e0d98cded06485f377
brookeperkins <brookemmperkins@gmail.com>
2021-02-15 15:34:13 -0800

    why not

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-// import functions and grab DOM elements
-import { translateToAYes } from './utils.js'
+import { translateToAYes } from './yes.js'
+const quizButton = document.getElementById('button');
+const scoreBox = document.getElementById('scorebox');
 
-const button = document.getElementById('button');
-console.log ()
-const result = document.getElementById('result');
-console.log()
-
-// initialize state
-
-// set event listeners to update state and DOM
 button.addEventListener('click', () => {
-    const name = prompt('What is your name?');
-    console.log(name);
- 
-    const confirm = confirm('Are you really ready for this?')
+    const name = prompt('What is your name?'); 
+    if (!confirm('Are you really ready for this?')) return;
+
+    let score = 0;
+  
+    const question1 = prompt('Is Nomi four years old?');
+    const question2 = prompt('Is Leaf a black cat?');
+    const question3 = prompt('Is Nomi a Leo?');
+    const question4 = prompt('Is Leaf into tuna?');
+
+    if (translateToAYes(question1)) score++;
+    if (!translateToAYes(question2)) score++;
+    if (translateToAYes(question3)) score++;
+    if (translateToAYes(question4)) score++;
+    alert('You made it through!');
     
-}
+    const correctGuesses = (score / 4) * 100;
+    const percentCorrect = Math.round(correctGuesses);
+    
+    quizButton.style.display = 'none';
+    scoreBox.style.display = 'block';
+        
+    if (score > 1) {
+        scoreBox.textContent = 
+        `Cat cuddles for life, ${name}! 
+        You scored ${percentCorrect}%! 🎉`;
+    } else {
+        scoreBox.textContent = 
+        `Okay ${name}, you scored ${percentCorrect}% 
+        which is... I mean, it's fine. Whatever. 😿`;
+    }
+});
+
